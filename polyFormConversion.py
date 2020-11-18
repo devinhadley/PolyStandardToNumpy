1591f1f3abd280abff55865789322ca1b200d834
Devin Hadley <devinhadley4@gmail.com>
2020-11-18 15:41:03 -0800

    coefficents are now floats, exponets can be any number.

--- polyFormConversion.py
+++ polyFormConversion.py
@@ -8,15 +8,32 @@ def getNum(string, i):
         change = i - counter
         if abs(change) != change:
             break
-        if string[change].isdigit() or string[change] == '-':
-            value = value + string[i-counter]
+        if string[change].isdigit() or string[change] == '-' or string[change] == '.':
+            value = value + string[change]
             counter = counter + 1
         else:
             break
     if value != "":
-        return int(value[::-1])
+        return float(value[::-1])
     else:
         return 1
+
+def getExponet(string, i):
+    counter = 1
+    value = ""
+    while True:
+        change = i + counter 
+        print(string[change])
+        if string[change].isdigit():
+            value = value + string[change]
+            counter = counter + 1
+        else:
+            break
+
+    if value != "":
+         return int(value)
+    else:
+         return 1
 
 
 
@@ -39,20 +56,17 @@ def setDegrees(string):
 
             if string[i+1] == "^":
 
-                print("Current degree:", current_degree)
-                print("Current string I + 1:", string[i+2])
-                print("Subtraction:", int(current_degree)-1)
                 if current_degree != 11111:
                     if int(string[i+2]) != int(current_degree) - 1:
                         difference =  int(current_degree) - int(string[i+2])
-                        current_degree = int(string[i+2])
+                        current_degree = getExponet(string, i+1)
                         for i in range(difference-1): # difference might be difference - 1 
                             current_coefficent.append(0)
 
                     else:
-                        current_degree = string[i+2]
+                        current_degree = getExponet(string, i+1)
                 else:
-                    current_degree = string[i+2]
+                    current_degree = getExponet(string, i+1)
                 current_coefficent.append("y")
 
     if string[len(string)-1].isdigit():
@@ -75,12 +89,12 @@ def setCoefficents(string):
         
         if char == "x":
             if string[i-1].isdigit():
-                coefficent_values.append(int(getNum(string, i)))
+                coefficent_values.append(getNum(string, i))
             else:
-                coefficent_values.append(1)
+                coefficent_values.append(1.0)
 
     if string[len(string)-1].isdigit():
-        coefficent_values.append(int(getNum(string, len(string))))
+        coefficent_values.append(getNum(string, len(string)))
 
     for num, char in enumerate(current_coefficent):
         if char == "y":
@@ -92,8 +106,8 @@ def setCoefficents(string):
     return np.poly1d(current_coefficent)
 
 
-x = setCoefficents("x^3+-10x^2+29x+-56")
-y = setCoefficents("x+-7")
+x = setCoefficents("2x^3+7x^2+-4x+7")
+y = setCoefficents("x^2+2x+-1")
 
 quotient, remainder = np.polydiv(x, y) 
   
